ed0646bfc96ddbe6543d9318a19baadb8b02ec34
Howl <github@thehowl.it>
2015-04-06 09:36:29 +0200

    KONAMI

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
+// dem global vars
+var isKonaming = false;
+
 // Shit menu button script
 function showMenu() {
   document.getElementById("site-menu").className = "";
-  document.getElementById("site-menu").className = "is-visible";
+  if (isKonaming) {
+    document.getElementById("site-menu").className = "is-visible ko fa-spin";
+  }
+  else {
+    document.getElementById("site-menu").className = "is-visible ko";
+  }
   document.getElementById("menubutton").className = "";
   document.getElementById("menubutton").className = "fa fa-bars quadbutton is-hidden";
 }
 function hideMenu() {
   document.getElementById("site-menu").className = "";
-  document.getElementById("site-menu").className = "is-hidden";
+  if (isKonaming) {
+    document.getElementById("site-menu").className = "is-hidden ko fa-spin";
+  }
+  else {
+    document.getElementById("site-menu").className = "is-hidden ko";
+  }
   document.getElementById("menubutton").className = "";
-  document.getElementById("menubutton").className = "fa fa-bars quadbutton";
+  document.getElementById("menubutton").className = "fa fa-bars quadbutton ko";
 }
 
 // Shit play/Pause button
@@ -18,11 +31,21 @@ function playPause() {
   if (mediaPlayer.paused) {
     mediaPlayer.play();
     document.getElementById("pause-button").className = "";
-    document.getElementById("pause-button").className = "fa fa-pause quadbutton";
+    if (isKonaming) {
+      document.getElementById("pause-button").className = "fa fa-pause quadbutton ko fa-spin";
+    }
+    else {
+      document.getElementById("pause-button").className = "fa fa-pause quadbutton ko";
+    }
   } else {
     mediaPlayer.pause();
     document.getElementById("pause-button").className = "";
-    document.getElementById("pause-button").className = "fa fa-play quadbutton";
+    if (isKonaming) {
+      document.getElementById("pause-button").className = "fa fa-play quadbutton ko fa-spin";
+    }
+    else {
+      document.getElementById("pause-button").className = "fa fa-play quadbutton ko";
+    }
   }
 }
 
@@ -39,10 +62,20 @@ var autonext = false;
 var toggleAutonext = function() {
   autonext = !autonext;
   if (autonext) {
-    $('#autonext').attr('class', 'fa fa-toggle-on quadbutton');
+    if (isKonaming) {
+      $('#autonext').attr('class', 'fa fa-toggle-on quadbutton ko fa-spin');
+    }
+    else {
+      $('#autonext').attr('class', 'fa fa-toggle-on quadbutton ko');
+    }
     $('video').removeAttr('loop');
   } else {
-    $('#autonext').attr('class', 'fa fa-toggle-off quadbutton');
+    if (isKonaming) {
+      $('#autonext').attr('class', 'fa fa-toggle-off quadbutton ko fa-spin');
+    }
+    else {
+      $('#autonext').attr('class', 'fa fa-toggle-off quadbutton ko');
+    }
     $('video').attr('loop', '');
   }
 }
@@ -84,3 +117,70 @@ function showTooltip(content) {
 function hideTooltip() {
   document.getElementById("tooltip").className = "is-hidden";
 }
+
+/*
+ * Konami Code For jQuery Plugin
+ * 1.3.0, 7 March 2014
+ *
+ * Using the Konami code, easily configure and Easter Egg for your page or any element on the page.
+ *
+ * Copyright 2011 - 2014 Tom McFarlin, http://tommcfarlin.com
+ * Released under the MIT License
+ */
+
+(function ( $ ) {
+  "use strict";
+
+  $.fn.konami = function( options ) {
+    var opts, controllerCode;
+    
+    opts = $.extend({}, $.fn.konami.defaults, options);
+    controllerCode = [];
+    
+    // note that we use the passed-in options, not the resolved options
+    opts.eventProperties = $.extend({}, options,  opts.eventProperties);
+    
+    this.keyup(function( evt ) {
+      var code = evt.keyCode || evt.which;
+
+      if ( opts.code.length > controllerCode.push( code ) ) {
+        return;
+      } // end if
+
+      if ( opts.code.length < controllerCode.length ) {
+        controllerCode.shift();
+      } // end if
+
+      if ( opts.code.toString() !== controllerCode.toString() ) {
+        return;
+      } // end if
+
+      opts.cheat(evt, opts);
+
+    }); // end keyup
+    
+    return this;
+  }; // end opts
+
+  $.fn.konami.defaults = {
+    code : [38,38,40,40,37,39,37,39,66,65],
+    eventName : 'konami',
+    eventProperties : null,
+    cheat: function(evt, opts) {
+      $(evt.target).trigger(opts.eventName, [ opts.eventProperties ]);
+    }
+  };
+
+}( jQuery ));
+
+// le konami code easter egg
+// why fa-spin? Because it saves bandwith by not creating an identical class with the same stuff!!!
+// (aka: i'm lazy)
+// --howl
+
+$(window).konami({
+  cheat: function() {
+    isKonaming = !isKonaming;
+    $('.ko').toggleClass('fa-spin');
+  }
+});
