9ec64fa6668457d881dca3872fdfb02b95ed1135
Dominik V. Salonen <domen178+gh@gmail.com>
2015-04-08 22:13:21 +0200

    About fucking time Quad

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ else { //Else, pick a random video
     <meta charset="utf-8">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Anime opening</title>
+    <title>Anime openings</title>
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 
   </head>
