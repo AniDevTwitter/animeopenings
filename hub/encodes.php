d39b26b3188c2000f115a26133e89fbbb5e8edbb
Dominik V. Salonen <hi@quad.moe>
2015-04-18 14:14:21 +0200

    Oh yeah, this page too

--- hub/encodes.php
+++ hub/encodes.php
@@ -16,10 +16,11 @@
 		<ul>
 		<li>Videos must not be over 40 MB unless there is a special reason</li>
 		<li>Videos may not exceed 3,5 mbit/s (Preferably below 3 mbit/s)</li>
+		<li>Videos must be 720p (No 1080p) or equal to the source material if no 720p+ source is available</li>
 		<li>Videos must be WebM files encoded with the VP8 or VP9 codec and Vorbis audio</li>
 		<li>Videos may not contain subtitle tracks (Firefox doesn't cooperate well with these)</li>
 		<li>The quality of the video must be acceptable. (Your submission will be checked for artifacts and blockiness)</li>
-		<li>Using the encoding settings provided <a href="">here</a> does <strong>not</strong> guarantee that your encode will be perfect</li>
+		<li>Using the encoding settings provided <a href="https://github.com/AniDevTwitter/animeopenings/blob/master/encoding-settings">here</a> does <strong>not</strong> guarantee that your encode will be perfect</li>
 		</ul>
 
 		<p>If you need a place to upload your file. I recommend <a href="http://uguu.se/">Uguu</a> or <a href="http://file.quad.moe">QuadFile</a>. Again, to submit encodes. Tweet a link to <a href="https://twitter.com/QuadPiece/">@QuadPiece</a>.</p>
@@ -27,7 +28,7 @@
 		<p>That might seem like a mouthful. But to keep the site running fast and users happy without exceeding our budget, some limits must be in place.</p>
 
 		<?php
-		include_once('../backend/includes/botnet.php');
+		include_once('../backend/includes/botnet.html');
 		?>
 
 	</body>
