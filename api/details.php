a4185d0ea45fac7eafa0a2927914d4f93bc7c47c
Dominik V. Salonen <hi@quad.moe>
2015-04-18 22:25:44 +0200

    Moar API

---
+++ api/details.php
@@ -0,0 +1,50 @@
+<?php
+
+$response = array();
+
+// Output JSON and kill script
+function output($output) {
+  echo json_encode($output);
+  die;
+}
+
+// Check if file is set
+if(!isset($_GET["file"])) {
+  $response["success"] = false;
+  $response["comment"] = "You did not specify a video file";
+  output($response);
+}
+
+// Set variables
+$video = $_GET["file"];
+$videolocation = '../video/' . $video;
+
+// Check if file exists
+if(!file_exists($videolocation)) {
+  $response["success"] = false;
+  $response["comment"] = "File does not exist";
+  output($response);
+}
+
+// Include the metadata list
+include_once('../names.php');
+
+// Check if the file is in the array
+if(!array_key_exists($video, $names)) {
+  $response["success"] = false;
+  $response["comment"] = "We do not have any metadata for this file";
+  output($response);
+}
+
+// If all test passed, Reply with information
+$data = $names[$video];
+
+//Set response
+$response["success"] = true;
+$response["comment"] = "No errors";
+$response["filename"] = $video;
+$response["title"] = $data["title"];
+$response["source"] = $data["source"];
+
+// Finish reply
+output($response);
--- hub/dev/api.php
+++ hub/dev/api.php
@@ -1,19 +1,56 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Notable people</title>
+		<title>API docs</title>
 		<link rel="stylesheet" type="text/css" href="../CSS/markdown.css">
 		<meta charset="UTF-8">
 	</head>
 	<body>
 
-    <h1 id="api">API</h1>
+		<h1 id="api">API</h1>
 
-    <p>There’s not much here yet. But more might arrive in the future</p>
+	<p>There’s not much here yet. But more might arrive in the future</p>
 
-    <h2 id="list-api">List API</h2>
+	<h2 id="list-api">List API</h2>
 
-    <p>Visit <code>http://openings.moe/api/list.php</code>, from there it should be obvious for anyone capable of recognizing JSON.</p>
+	<p>Visit <code>http://openings.moe/api/list.php</code>, from there it should be obvious for anyone capable of recognizing JSON.</p>
+
+	<h2 id="details-api">Details API</h2>
+
+
+
+	<h3 id="using-the-details-api">Using the details API</h3>
+
+	<p>Simply use <code>http://openings.moe/api/list.php?file=&lt;YOURFILE&gt;</code></p>
+
+	<p>The API will return the metadata for the file specified. It will also return a comment for both successful and failed API calls.</p>
+
+	<h3 id="samples">Samples</h3>
+
+
+
+	<h4 id="requests">Requests</h4>
+
+	<p>So to get the details of <a href="http://openings.moe/?video=Opening1-NoGameNoLife.webm">this video</a>. <br>
+	We would simply use <code>http://openings.moe/api/list.php?file=Opening1-NoGameNoLife.webm</code></p>
+
+	<h4 id="sample-reply">Sample reply</h4>
+
+	<p>The sample used above would return:</p>
+
+
+
+	<pre class="prettyprint"><code class=" hljs json">{"<span class="hljs-attribute">success</span>":<span class="hljs-value"><span class="hljs-literal">true</span></span>,"<span class="hljs-attribute">comment</span>":<span class="hljs-value"><span class="hljs-string">"No errors"</span></span>,"<span class="hljs-attribute">filename</span>":<span class="hljs-value"><span class="hljs-string">"Opening1-NoGameNoLife.webm"</span></span>,"<span class="hljs-attribute">title</span>":<span class="hljs-value"><span class="hljs-string">"Opening 1"</span></span>,"<span class="hljs-attribute">source</span>":<span class="hljs-value"><span class="hljs-string">"No Game No Life"</span></span>}</code></pre>
+
+	<p>The API will output a string like that. But here’s a “prettyfied” response you can use as a reference:</p>
+
+	<pre class="prettyprint"><code class=" hljs json">{
+	    "<span class="hljs-attribute">success</span>":<span class="hljs-value"><span class="hljs-literal">true</span></span>,
+	    "<span class="hljs-attribute">comment</span>":<span class="hljs-value"><span class="hljs-string">"No errors"</span></span>,
+	    "<span class="hljs-attribute">title</span>":<span class="hljs-value"><span class="hljs-string">"Opening 1"</span></span>,
+	    "<span class="hljs-attribute">source</span>":<span class="hljs-value"><span class="hljs-string">"No Game No Life"</span>
+	</span>
+	}</code></pre>
 
 		<?php
 		include_once('../../backend/includes/botnet.html');
