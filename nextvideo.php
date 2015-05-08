afed65164c452ee1a4c160c14ef7e2fa001e5264
Howl <github@thehowl.it>
2015-05-08 23:06:53 +0200

    avoid 2 same openings in a row

--- nextvideo.php
+++ nextvideo.php
@@ -3,12 +3,16 @@
 try {
     require_once "names.php";
     $videos = glob("video/*.webm");
-    $video = $videos[rand(0, count($videos) - 1)];
-    $out = new stdClass();
-    $out->success = true;
-    $out->videourl = $video;
-    $a = explode("/", $video);
-    $a = $a[1];
+    // do-while because we must do this at least once.
+    // this loop basically ensures that we aren't playing the same video twice in a row
+    do {
+        $video = $videos[rand(0, count($videos) - 1)];
+        $out = new stdClass();
+        $out->success = true;
+        $out->videourl = $video;
+        $a = explode("/", $video);
+        $a = $a[1];
+    } while (isset($_GET["avoid"]) && $a == $_GET["avoid"]);
     if (isset($names[$a])) {
         $out->videoname = $names[$a];
     }
