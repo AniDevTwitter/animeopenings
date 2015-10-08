628f95342ea17bb8e631fc21d2b0fb2096d11d02
Dominik V. Salonen <domen178@gmail.com>
2015-10-08 12:29:45 +0200

    Link to encoding page on GitHub

--- hub/encodes.php
+++ hub/encodes.php
@@ -16,21 +16,11 @@
 		</header>
 		<main>
 			<p>Currently, there’s no proper system for submitting encodes. However, if you have something you wish to submit you can contact Quad via <a href="https://twitter.com/QuadPiece/">Twitter</a>, or someone else through our <a href="http://qchat.rizon.net/?channels=aniop">IRC channel</a>.</p>
-			<p>There are a couple of things you should keep in mind when submitting encodes:</p>
+			<p>There are a couple of things you should keep in mind when submitting encodes</p>
 
-			<ul>
-				<li>Videos must not be over 40 MB unless there is a special reason.</li>
-				<li>Videos may not exceed 3,5 mbit/s (Preferably below 3 mbit/s).</li>
-				<li>Videos must be 720p (not 1080p), or equal to the source material if no 720p+ source is available.</li>
-				<li>Videos must be WebM files encoded with the VP9 codec and Vorbis audio.</li>
-				<li>Videos may not contain subtitle tracks (Firefox doesn't cooperate well with these).</li>
-				<li>The quality of the video must be acceptable. Your submission will be checked for artifacts and blockiness.</li>
-				<li>Using the encoding settings provided <a href="https://github.com/AniDevTwitter/animeopenings/blob/master/encoding-settings">here</a> does <strong>not</strong> guarantee that your encode will be perfect.</li>
-			</ul>
+			<p>The encoding details have been <a href="">moved to GitHub</a></p> 
 
 			<p>If you need a place to upload your file, I recommend <a href="http://uguu.se/">Uguu</a>.</p>
-
-			<p>That might seem like a mouthful, but to keep the site running fast and users happy without exceeding our budget, some limits must be in place.</p>
 		</main>
 
 		<?php
