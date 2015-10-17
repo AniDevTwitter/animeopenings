70906e3efefd7dabbe9ab3687ef43b1beff4b7af
Dominik V. Salonen <QuadPiece@users.noreply.github.com>
2015-10-17 16:31:49 +0200

    Fixed typo

--- list/index.php
+++ list/index.php
@@ -62,7 +62,7 @@
 			foreach ($video_array as $video) {
 				echo '	<i class="fa fa-plus"';
 					if (isset($video["song"])) echo ' songTitle="' . $video["song"]["title"] . '" songArtist="' . $video["song"]["artist"] . '"';
-					if (isset($video["subtitles"] && $video["subtitles"] != 0)) echo ' subtitles="' . $video["subtitles"] . '"';
+					if (isset($video["subtitles"]) && $video["subtitles"] != 0) echo ' subtitles="' . $video["subtitles"] . '"';
 				echo '></i>' . PHP_EOL;
 				echo '	<a href="../?video=' . $video["filename"] . '">' . $video["title"] . "</a>" . PHP_EOL;
 				echo "	<br />" . PHP_EOL;
