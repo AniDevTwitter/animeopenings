e508ce770026e9253d827b9e32e7498780a5870d
Yay295 <Yay295@users.noreply.github.com>
2015-11-14 20:05:10 -0700

    Update chat.php
    Moved css to head, fixed some grammar, and removed duplicate title. Also removed message about the chat being new.

--- hub/chat.php
+++ hub/chat.php
@@ -5,6 +5,17 @@
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="../CSS/page.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<style>
+		iframe {
+			border: none;
+			width:100%;
+			height:80vh;
+		}
+		p {
+			font-size:65%;
+			line-height:1em;
+		}
+		</style>
 	</head>
 	<body>
 		<header>
@@ -14,18 +25,13 @@
 			</div>
 		</header>
 		<main>
-			<h3>Chat</h3>
+			<iframe src="https://kiwiirc.com/client?settings=58bedb49a748db1de84a0412a3411757"></iframe>
 
-			<p style="color:#555;font-size:65%;">This chat is pretty new so there might not be many users online at the moment</p>
-
-			<iframe src="https://kiwiirc.com/client?settings=58bedb49a748db1de84a0412a3411757" style="border:none;width:100%; height:80vh;"></iframe>
-
-			<p style="font-size:65%;line-height:1em;">The default channel is #openings.moe, try #aniop for contributors, details about our channels can be found <a href="ircdetails.php">here</a>. You can also <a href="https://kiwiirc.com/client?settings=58bedb49a748db1de84a0412a3411757">open the client in a new tab</a></p>
-
+			<p>The default channel is #openings.moe; try #aniop for contributors. Details about our channels can be found <a href="ircdetails.php">here</a>. You can also <a href="https://kiwiirc.com/client?settings=58bedb49a748db1de84a0412a3411757">open the client in a new tab</a>.</p>
 		</main>
 
 		<?php
-		include_once('../backend/includes/botnet.html');
+		include_once "../backend/includes/botnet.html";
 		?>
 	</body>
 </html>
