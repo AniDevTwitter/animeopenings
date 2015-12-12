ae74172bb8c4606443a0079d7fec12965d4bc981
Yay295 <Yay295@users.noreply.github.com>
2015-12-12 04:08:21 -0700

    updated for new names.php
    also added subtitles to output, and removed song/subtitles if they weren't set

--- api/list.php
+++ api/list.php
@@ -1,14 +1,14 @@
 <?php
 function keySearch($array1, $field, $value) {
-   foreach($array1 as $key => $array2) {
-      if ($array2[$field] === $value)
-         return $key;
-   }
-   return false;
+	foreach($array1 as $key => $array2) {
+		if ($array2[$field] === $value)
+			return $key;
+	}
+	return false;
 }
 
-include_once("../names.php");
-include_once("../eggs.php");
+include_once "../names.php";
+include_once "../eggs.php";
 
 if (isset($_GET["eggs"])) $videos = $names + $eggs;
 else $videos = $names;
@@ -16,37 +16,68 @@ else $videos = $names;
 $output = array();
 
 if (isset($_GET["filenames"])) {
-  foreach ($videos as $key => $opening) {
-    $output[] = $key;
-  }
+	foreach ($videos as $series => $video_array) {
+		foreach ($video_array as $title => $data) {
+			$output[] = $data["file"];
+		}
+	}
+
+	if (isset($_GET["shuffle"])) shuffle($output);
+
+	if (isset($_GET["first"])) {
+		$key = array_search($_GET["first"], $output);
+		unset($output[$key]);
+		array_unshift($output, $_GET["first"]);
+	}
 } else {
-  foreach ($videos as $key => $opening) {
-    $output[] = array(
-      "title" => $opening["title"],
-      "source" => $opening["source"],
-      "song" => (isset($opening["song"]) ? $opening["song"] : 0),
-      "subtitles" => (isset($opening["subtitles"]) ? $opening["subtitles"] : 0),
-      "file" => $key
-    );
-  }
+	if (isset($_GET["shuffle"])) {
+		while (!empty($videos)) {
+			$series = array_rand($videos);
+			$title = array_rand($videos[$series]);
+
+			$data = &$videos[$series][$title];
+			$output[] = [
+				"title" => $title,
+				"source" => $series,
+				"song" => (array_key_exists("song", $data) ? $data["song"] : 0),
+				"subtitles" => (array_key_exists("subtitles", $data) ? $opening["subtitles"] : 0),
+				"file" => $data["file"]
+			];
+
+			end($output);
+			$last = &$output[key($output)];
+			if ($last["song"] == 0) unset($last["song"]);
+			if ($last["subtitles"] == 0) unset($last["subtitles"]);
+
+			unset($videos[$series][$title]);
+			if (empty($videos[$series])) unset($videos[$series]);
+		}
+	} else {
+		foreach ($videos as $series => $video_array) {
+			foreach ($video_array as $title => $data) {
+				$output[] = [
+					"title" => $title,
+					"source" => $series,
+					"song" => (array_key_exists("song", $data) ? $data["song"] : 0),
+					"subtitles" => (array_key_exists("subtitles", $data) ? $opening["subtitles"] : 0),
+					"file" => $data["file"]
+				];
+
+				end($output);
+				$last = &$output[key($output)];
+				if ($last["song"] == 0) unset($last["song"]);
+				if ($last["subtitles"] == 0) unset($last["subtitles"]);
+			}
+		}
+	}
+
+	if (isset($_GET["first"])) {
+		$key = keySearch($output, "file", $_GET["first"]);
+		$first = $output[$key];
+		unset($output[$key]);
+		array_unshift($output, $first);
+	}
 }
 
-if (isset($_GET["shuffle"])) shuffle($output);
-
-// move $_GET["first"] to the front of $output
-if (isset($_GET["first"])) {
-  if (isset($_GET["filenames"])) {
-    $key = array_search($_GET["first"], $output);
-    unset($output[$key]);
-    array_unshift($output, $_GET["first"]);
-  } else {
-    $key = keySearch($output, "file", $_GET["first"]);
-    $first = $output[$key];
-    unset($output[$key]);
-    array_unshift($output, $first);
-  }
-}
-
-header('Content-Type: application/json');
 echo json_encode($output);
 ?>
