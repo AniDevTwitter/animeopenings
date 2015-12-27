dd5d61346a45c4b06a7d5f2b6c6df3ef776908b0
Yay295 <Yay295@users.noreply.github.com>
2015-12-27 13:16:45 -0600

    Update aniop.bat
    set start and end to 0 if not set
    (ET==0) == ("encode to the end")

--- encode/Yay295-s-Windows-Version/aniop.bat
+++ encode/Yay295-s-Windows-Version/aniop.bat
@@ -14,6 +14,9 @@ SET THREADS=4
 
 SET COMMAND="%1"
 
+IF NOT DEFINED SS SET SS=0
+IF NOT DEFINED ET SET ET=0
+
 IF %COMMAND% == "" (
   ECHO Commands:
   ECHO test   - Test your start and end times without audio.
@@ -23,30 +26,34 @@ IF %COMMAND% == "" (
   ECHO.
   ECHO Required Variable Descriptions:
   ECHO FILE = The relative directory to the input video file.
-  ECHO SS   = The opening/ending start time.
-  ECHO ET   = The time elapsed since the start time.
+  ECHO SS   = The opening/ending start time. [default = start = 0]
+  ECHO ET   = The time elapsed since the start time. [default = end = 0]
   ECHO OFN  = The relative directory to the output video file.
 )
 
 IF %COMMAND% == "test" (
-  ffmpeg -ss %SS% -i %FILE% -t %ET% -c:v libvpx-vp9 -quality realtime -speed 4 -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -an -sn -y "TEST %OFN%.webm"
+  IF !ET! == 0 ( echo ffmpeg -ss !SS! -i %FILE% -c:v libvpx-vp9 -quality realtime -speed 4 -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -an -sn -y "TEST %OFN%.webm"
+  ) ELSE echo ffmpeg -ss !SS! -i %FILE% -t !ET! -c:v libvpx-vp9 -quality realtime -speed 4 -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -an -sn -y "TEST %OFN%.webm"
 )
 
 IF %COMMAND% == "testa" (
-  ffmpeg -ss %SS% -i %FILE% -t %ET% -c:v libvpx-vp9 -quality realtime -speed 4 -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -sn -y "TEST %OFN%.webm"
+  IF !ET! == 0 ( ffmpeg -ss !SS! -i %FILE% -c:v libvpx-vp9 -quality realtime -speed 4 -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -sn -y "TEST %OFN%.webm"
+  ) ELSE ffmpeg -ss !SS! -i %FILE% -t !ET! -c:v libvpx-vp9 -quality realtime -speed 4 -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -sn -y "TEST %OFN%.webm"
 )
 
 IF %COMMAND% == "volume" (
   SET VOLUME=     ???
   ECHO Maximum Volume:
-  ffmpeg -ss %SS% -i %FILE% -t %ET% -af volumedetect -f null NUL 2> %OFN%.vol1
+  IF !ET! == 0 ( ffmpeg -ss !SS! -i %FILE% -af volumedetect -f null NUL 2> %OFN%.vol1
+  ) ELSE ffmpeg -ss !SS! -i %FILE% -t !ET! -af volumedetect -f null NUL 2> %OFN%.vol1
   FOR /F "skip=30 tokens=4,5" %%i IN ( %OFN%.vol1 ) DO (
     IF "%%i" == "max_volume:" SET VOLUME=     %%j
   )
   DEl %OFN%.vol1
   ECHO Original = !VOLUME:~-5!dB
   SET VOLUME=     ???
-  ffmpeg -i %OFN%.webm -af volumedetect -f null NUL 2> %OFN%.vol2
+  IF !ET! == 0 ( ffmpeg -i %OFN%.webm -af volumedetect -f null NUL 2> %OFN%.vol2
+  ) ELSE ffmpeg -i %OFN%.webm -af volumedetect -f null NUL 2> %OFN%.vol2
   FOR /F "skip=30 tokens=4,5" %%i IN ( %OFN%.vol2 ) DO (
     IF "%%i" == "max_volume:" SET VOLUME=     %%j
   )
@@ -56,7 +63,8 @@ IF %COMMAND% == "volume" (
 
 IF %COMMAND% == "encode" (
   ECHO Getting Max Volume - !time!
-  ffmpeg -ss %SS% -i %FILE% -t %ET% -af volumedetect -f null NUL 2> %OFN%.log
+  IF !ET! == 0 ( ffmpeg -ss !SS! -i %FILE% -af volumedetect -f null NUL 2> %OFN%.log
+  ) ELSE ffmpeg -ss !SS! -i %FILE% -t !ET! -af volumedetect -f null NUL 2> %OFN%.log
   FOR /F "skip=30 tokens=4,5" %%i IN ( %OFN%.log ) DO (
     IF "%%i" == "max_volume:" SET VOLUME=%%j
   )
@@ -66,16 +74,18 @@ IF %COMMAND% == "encode" (
   REM Normalize to 0dB (remove the '-' from %VOLUME%)
   REM SET VOLUME=!VOLUME:~1!
 
-  REM Normalize to -0.4dB (seems to normalize to 0 though).
+  Normalize to -0.4dB (seems to normalize to 0 though).
   PowerShell -C !VOLUME!*-1-0.4 > %OFN%.tmp
   SET /P VOLUME=<%OFN%.tmp
   DEl %OFN%.tmp
 
   ECHO.    Adjusting Volume by !VOLUME!dB
   ECHO Pass 1             - !time!
-  START /W CMD /C ffmpeg -ss %SS% -i %FILE% -t %ET% -pass 1 -c:v libvpx-vp9 -b:v %VBITRATE% -maxrate %VMAXRATE% -speed %SPEED% -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -an -sn -f webm -y -passlogfile %OFN% NUL
+  IF !ET! == 0 ( START /W CMD /C ffmpeg -ss !SS! -i %FILE% -pass 1 -c:v libvpx-vp9 -b:v %VBITRATE% -maxrate %VMAXRATE% -speed %SPEED% -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -an -sn -f webm -y -passlogfile %OFN% NUL
+  ) ELSE START /W CMD /C ffmpeg -ss !SS! -i %FILE% -t !ET! -pass 1 -c:v libvpx-vp9 -b:v %VBITRATE% -maxrate %VMAXRATE% -speed %SPEED% -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -an -sn -f webm -y -passlogfile %OFN% NUL
   ECHO Pass 2             - !time!
-  START /W CMD /C ffmpeg -ss %SS% -i %FILE% -t %ET% -pass 2 -c:v libvpx-vp9 -b:v %VBITRATE% -maxrate %VMAXRATE% -speed %SPEED% -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -af volume=!VOLUME!dB -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libvorbis -b:a 192k -sn -y -passlogfile %OFN% %OFN%.webm
+  IF !ET! == 0 ( START /W CMD /C ffmpeg -ss !SS! -i %FILE% -pass 2 -c:v libvpx-vp9 -b:v %VBITRATE% -maxrate %VMAXRATE% -speed %SPEED% -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -af volume=!VOLUME!dB -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libvorbis -b:a 192k -sn -y -passlogfile %OFN% %OFN%.webm
+  ) ELSE START /W CMD /C ffmpeg -ss !SS! -i %FILE% -t !ET! -pass 2 -c:v libvpx-vp9 -b:v %VBITRATE% -maxrate %VMAXRATE% -speed %SPEED% -g %G% -slices %SLICES% -vf "scale=-1:min(720\,ih)" -af volume=!VOLUME!dB -threads %THREADS% -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 -c:a libvorbis -b:a 192k -sn -y -passlogfile %OFN% %OFN%.webm
   DEL %OFN%-0.log
   ECHO Done               - !time!
 )
