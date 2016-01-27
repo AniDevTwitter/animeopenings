b7c9c2550ec49df3fd50f4e9e2b13f550ffb7f7d
Dominik V. Salonen <QuadPiece@users.noreply.github.com>
2016-01-27 12:12:30 +0100

    Added font awesome to F.A.Q page
    The keybinds need it.

--- hub/faq.php
+++ hub/faq.php
@@ -5,6 +5,7 @@
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="../CSS/page.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<style>
 			.keycap {
 				border: 1px solid rgba(0, 0, 0, 0.2);
