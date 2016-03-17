3ad73a86e6d2e0d5654a4afa31b0af0e5baee2ee
Dominik V. Salonen <QuadPiece@users.noreply.github.com>
2016-03-17 09:48:52 +0100

    Welcome to 2016, the year of bitcoins

--- hub/donate.php
+++ hub/donate.php
@@ -13,7 +13,8 @@
 				padding: 5px;
 				text-align: center;
 				transition: all 0.325s;
-				width: 320px;
+				width: 250px;
+				max-width: 100%;
 			}
 
 			.button:hover {
@@ -21,7 +22,11 @@
 				box-shadow: 0px 2px 0px #111;
 			}
 
-			.small { font-size: 10pt; }
+			.small { 
+			  font-size: 10pt; 
+			  display: block;
+			  line-height: 1.5em;
+			}
 		</style>
 	</head>
 
@@ -38,6 +43,9 @@
 			<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WGJ33B4E7YV9N&lc=US&item_name=Openings%2emoe&item_number=1337&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted">
 				<div class="button">PayPal</div>
 			</a>
+			<a href="https://www.coinbase.com/quadpiece">
+				<div class="button">Bitcoin*</div>
+			</a>
 			<a href="https://docs.google.com/spreadsheets/d/148jM7WzF2RCW_hXCU3OJ0mwNPp8TqgjFXMal-ytQhWg/edit?usp=sharing">
 				<div class="button">Transparency</div>
 			</a>
@@ -49,7 +57,7 @@
 			<ul>
 				<li>The main server: <b>$10 per month</b></li>
 				<li>Netherlands server: <b>$10 per month</b></li>
-				<li>France server: <b>€2.99 per month</b></li>
+				<li>France server: <b>€11.99 per month</b></li>
 				<li>US server: <b>$10 per month</b></li>
 				<li>DNS: <b>~€3 per month (varies)</b></li>
 			</ul>
@@ -63,7 +71,7 @@
 			<h2>Things we want money for</h2>
 
 			<ul>
-				<li>Better server in Germany to replace eu1: <b>$15 per month</b></li>
+				<li>Better server in the US: <b>???</b></li>
 				<li>Asian server: <b>$5 per month</b> or more</li>
 			</ul>
 
@@ -72,8 +80,19 @@
 			<ul>
 				<li>Dedicated multi-core encoding server: <b>$50-ish per month</b></li>
 			</ul>
-
-			<i class="small">* A dollar or two of your donation might get spent on coffee to power late night encoding sessions.</i>
+			
+			<i class="small">
+			  * Bitcoin payments are processed using coinbase. Please include "openings.moe" somewhere in the message
+			  and optionally a name that will be used to credit your donation in the sheet. If no name is given, it 
+			  will be marked "Anonymous".
+			  <br>
+			  If you prefer, you can use this address to pay: 1HBWgdHj17P1fSF9iDDjzzH1EfeQopF6T8
+			  <br>
+			  Bitcoin donations will be converted to USD at the current rate as soon as I add them to my paperwork.
+			  This is just for consistency, I will cover losses/gains due to fluctuations out of my own pocket. Shit,
+			  this note is getting really long now, I think it's about time I stopped writing.
+			</i>
+			
 		</main>
 
 		<?php
