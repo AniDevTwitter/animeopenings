51132f159dcf73b1924eca5048d862530c8690d4
Yay295 <Yay295@users.noreply.github.com>
2016-12-16 00:03:47 -0600

    fix color start indices

--- subtitles/code/subtitles.js
+++ subtitles/code/subtitles.js
@@ -1024,36 +1024,36 @@ function subtitleRenderer(SC, video, subFile) {
 		if (style.BackColour.length == 1) style.BackColour = "000000";
 
 		if (style.PrimaryColour.length == 8) {
-			style.c1a = (255-parseInt(style.PrimaryColour.substr(2,2),16))/255;
+			style.c1a = (255-parseInt(style.PrimaryColour.substr(0,2),16))/255;
 			style.PrimaryColour = style.PrimaryColour.slice(2);
 		} else style.c1a = 1;
-		style.c1r = parseInt(style.PrimaryColour.substr(6,2),16);
-		style.c1g = parseInt(style.PrimaryColour.substr(4,2),16);
-		style.c1b = parseInt(style.PrimaryColour.substr(2,2),16);
+		style.c1r = parseInt(style.PrimaryColour.substr(4,2),16);
+		style.c1g = parseInt(style.PrimaryColour.substr(2,2),16);
+		style.c1b = parseInt(style.PrimaryColour.substr(0,2),16);
 
 		if (style.SecondaryColour.length == 8) {
-			style.c2a = (255-parseInt(style.SecondaryColour.substr(2,2),16))/255;
+			style.c2a = (255-parseInt(style.SecondaryColour.substr(0,2),16))/255;
 			style.SecondaryColour = style.SecondaryColour.slice(2);
 		} else style.c2a = 1;
-		style.c2r = parseInt(style.SecondaryColour.substr(6,2),16);
-		style.c2g = parseInt(style.SecondaryColour.substr(4,2),16);
-		style.c2b = parseInt(style.SecondaryColour.substr(2,2),16);
+		style.c2r = parseInt(style.SecondaryColour.substr(4,2),16);
+		style.c2g = parseInt(style.SecondaryColour.substr(2,2),16);
+		style.c2b = parseInt(style.SecondaryColour.substr(0,2),16);
 
 		if (style.OutlineColour.length == 8) {
-			style.c3a = (255-parseInt(style.OutlineColour.substr(2,2),16))/255;
+			style.c3a = (255-parseInt(style.OutlineColour.substr(0,2),16))/255;
 			style.OutlineColour = style.OutlineColour.slice(2);
 		} else style.c3a = 1;
-		style.c3r = parseInt(style.OutlineColour.substr(6,2),16);
-		style.c3g = parseInt(style.OutlineColour.substr(4,2),16);
-		style.c3b = parseInt(style.OutlineColour.substr(2,2),16);
+		style.c3r = parseInt(style.OutlineColour.substr(4,2),16);
+		style.c3g = parseInt(style.OutlineColour.substr(2,2),16);
+		style.c3b = parseInt(style.OutlineColour.substr(0,2),16);
 
 		if (style.BackColour.length == 8) {
-			style.c4a = (255-parseInt(style.BackColour.substr(2,2),16))/255;
+			style.c4a = (255-parseInt(style.BackColour.substr(0,2),16))/255;
 			style.BackColour = style.BackColour.slice(2);
 		} else style.c4a = 1;
-		style.c4r = parseInt(style.BackColour.substr(6,2),16);
-		style.c4g = parseInt(style.BackColour.substr(4,2),16);
-		style.c4b = parseInt(style.BackColour.substr(2,2),16);
+		style.c4r = parseInt(style.BackColour.substr(4,2),16);
+		style.c4g = parseInt(style.BackColour.substr(2,2),16);
+		style.c4b = parseInt(style.BackColour.substr(0,2),16);
 
 
 		if (!style.Angle) style.Angle = 0;
