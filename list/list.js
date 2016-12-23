518072ab1f0bc6441cdb251a7a65b9c6f8b01937
Yay295 <Yay295@users.noreply.github.com>
2016-12-23 10:31:49 -0600

    move inline js to js file

--- list/list.js
+++ list/list.js
@@ -5,6 +5,9 @@ let RegExEnabled = false;
 // polyfill for noncompliant browsers
 if (!(HTMLCollection.prototype[Symbol.iterator])) HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 
+document.addEventListener("load", setup);
+document.addEventListener("pageshow", search);
+
 function setup() {
 	// get list of series elements and set their id
 	list = document.getElementsByClassName("series");
