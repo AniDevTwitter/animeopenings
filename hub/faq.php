987384562a6b296492ef973925aff7915d8c10d0
Yay295 <Yay295@gmail.com>
2018-06-23 22:14:23 -0500

    update faq

--- hub/faq.php
+++ hub/faq.php
@@ -18,11 +18,11 @@
 		<header>
 			<div>
 				<h1>Frequently Asked Questions</h1>
-				<?php include "navbar"; ?>
+				<?php include 'navbar'; ?>
 			</div>
 		</header>
 		<main>
-			<h3 id="there-are-endings-too">Why do you lie to me? You're named openings.moe but you also have endings.</h3>
+			<h3 id="there-are-endings-too">Why do you lie to me? You're named openings.moe but you also have endings!</h3>
 
 			<p>Well excuse me, but "openingsandendings.moe" was a bit too long for my taste. You can click the circle in the bottom left to only get openings.</p>
 
@@ -30,22 +30,9 @@
 
 			<p>Openings will come. When you suggest something it's added to a list. Openings on this list are completed in a random order so someday your opening/ending will arrive. If it will take 30 minutes or 5 weeks however is something I can't tell you. And nagging does not help (in fact, it'll probably make me lower the priority).</p>
 
-			<h3 id="i-cant-wait-for-my-opening-to-arrive">I can't wait for my opening to arrive, you work like a snail.</h3>
-
-			<p>Then <a href="encodes.php">submit an encode yourself</a>, or find someone else willing to encode and submit it for you.</p>
-
 			<h3 id="why-wont-the-videos-play">Why won't the videos play?</h3>
 
-			<p>The most likely reasons are:</p>
-
-			<ul>
-				<li>You're using an iOS device.</li>
-				<li>You're using a browser (eg. Internet Explorer, Safari) that doesn't support WebM.</li>
-			</ul>
-
-			<p>If you are using an iOS device, there is nothing you can do except not use an iOS device.</p>
-
-			<p>If you are using a browser that doesn't support WebM, get one that does (eg. Firefox, Chrome, or Opera). If you cannot use another browser, there are plugins available somewhere online that will allow you to play WebM videos.</p>
+			<p>If you're using a new version of Chrome, you will need to click the page first to make it start. Chrome has decided to block autoplaying videos to stop spam, which unfortunately also breaks our site.</p>
 
 			<p>If you believe another issue is causing this, contact <a href="https://twitter.com/Kuwaddo/">Quad on Twitter</a> or alternatively by mail (<a href="mailto:contact@quad.moe">contact@quad.moe</a>) and we can try to work out a solution.</p>
 
@@ -55,7 +42,7 @@
 
 			<h3 id="i-got-this-really-weird-video">I got this really weird video, what does that mean?</h3>
 
-			<p>There are Easter Eggs hidden on the site. These will report the title and source as "???". Congratulations, you found one!</p>
+			<p>There are Easter Eggs hidden on the site. Congratulations, you found one!</p>
 
 			<h3 id="keybindings">Can I use my keyboard to interact with the site?</h3>
 
@@ -63,15 +50,13 @@
 
 			<p><span class="keycap">M</span> Open/Close menu</p>
 			<p><span class="keycap">N</span> New video</p>
+			<p><span class="keycap">S</span> Toggle subtitles (if available)</p>
 			<p><span class="keycap"><span class="fa fa-arrow-left"></span>/<span class="fa fa-arrow-right"></span></span> Back/Forward 10 seconds</p>
 			<p><span class="keycap">Space</span> Pause/Play</p>
 			<p><span class="keycap">F</span> Toggle fullscreen</p>
-			<p><span class="keycap">S</span> Toggle subtitles (experimental)</p>
 			<p><span class="keycap">Page Up/Down or Scroll Wheel</span> Volume</p>
 		</main>
 
-		<?php
-		include_once "../backend/includes/botnet.html";
-		?>
+		<?php include_once '../backend/includes/botnet.html'; ?>
 	</body>
 </html>
