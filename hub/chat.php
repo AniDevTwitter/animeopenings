220d7f2c114a5ecbb0884426670594880a2cc4b3
Yay295 <Yay295@gmail.com>
2018-06-23 20:26:35 -0500

    fix irc embed

--- hub/chat.php
+++ hub/chat.php
@@ -6,32 +6,26 @@
 		<link rel="stylesheet" type="text/css" href="../CSS/page.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<style>
-		iframe {
-			border: none;
-			width:100%;
-			height:80vh;
-		}
-		p {
-			font-size:65%;
-			line-height:1em;
-		}
+			iframe {
+				border: none;
+				width: 100%;
+				height: 75vh;
+			}
+			p {font-size:65%}
 		</style>
 	</head>
 	<body>
 		<header>
 			<div>
 				<h1>Chat</h1>
-				<?php include "navbar"; ?>
+				<?php include 'navbar'; ?>
 			</div>
 		</header>
 		<main>
-			<iframe src="https://kiwiirc.com/client?settings=58bedb49a748db1de84a0412a3411757"></iframe>
-
-			<p>Change your nick to something you like, the rest of the details don't matter. Details about our channels can be found <a href="ircdetails.php">here</a>. You can also <a href="https://irc-client.quad.moe/" target="_blank">open the client in a new tab</a>.</p>
+			<iframe src="https://qchat.rizon.net/?channels=openings.moe"></iframe>
+			<p>Change your nick to something you like, the rest of the details don't matter. Details about our channels can be found <a href="ircdetails.php">here</a>. You can also <a href="https://qchat.rizon.net/?channels=openings.moe" target="_blank">open the client in a new tab</a>.</p>
 		</main>
 
-		<?php
-		include_once "../backend/includes/botnet.html";
-		?>
+		<?php include_once '../backend/includes/botnet.html'; ?>
 	</body>
 </html>
