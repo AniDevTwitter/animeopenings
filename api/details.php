bbc68881a29cf133032da3bed8e097986bb65066
Yay295 <Yay295@gmail.com>
2018-07-07 16:07:37 -0500

    double quotes -> single quotes

--- api/details.php
+++ api/details.php
@@ -1,63 +1,63 @@
 <?php
-include "../backend/includes/helpers.php";
+include '../backend/includes/helpers.php';
 
 // Make an empty array
 $response = array();
 
 // Output JSON and kill script
 function output($output) {
-	header("Content-Type: application/json");
+	header('Content-Type: application/json');
 	echo json_encode($output);
 	die;
 }
 
 // Check if file is set
-if (!isset($_GET["file"])) {
-	$response["success"] = false;
-	$response["comment"] = "You did not specify a video file";
+if (!isset($_GET['file'])) {
+	$response['success'] = false;
+	$response['comment'] = 'You did not specify a video file';
 	output($response);
 }
 
-$filename = identifierToFilename($_GET["file"]);
+$filename = identifierToFilename($_GET['file']);
 $len = strlen($filename);
 
 // Include the metadata list
-include_once "../names.php";
+include_once '../names.php';
 
 // Check if the file is in the array
 $found = false;
 foreach ($names as $S => $video_array) {
 	foreach ($video_array as $T => $data) {
-		if (substr($data["file"], 0, $len) === $filename) {
+		if (substr($data['file'], 0, $len) === $filename) {
 			$found = true;
 			$series = $S;
 			$title = $T;
-			$filename = $data["file"];
-			$mime = $data["mime"];
-			$song = existsOrDefault("song", $data);
-			$subtitles = existsOrDefault("subtitles", $data);
-			$egg = existsOrDefault("egg", $data);
+			$filename = $data['file'];
+			$mime = $data['mime'];
+			$song = existsOrDefault('song', $data);
+			$subtitles = existsOrDefault('subtitles', $data);
+			$egg = existsOrDefault('egg', $data);
 			break 2;
 		}
 	}
 }
 
 if (!$found) {
-	$response["success"] = false;
-	$response["comment"] = "We do not have any metadata for this file";
+	$response['success'] = false;
+	$response['comment'] = 'We do not have any metadata for that file';
 	output($response);
 }
 
 // Set response
-$response["success"] = true;
-$response["comment"] = "No errors";
-$response["filename"] = $filename;
-$response["title"] = $title;
-$response["mime"] = $mime;
-$response["source"] = $series;
-if (!is_null($song)) $response["song"] = $song;
-if (!is_null($subtitles)) $response["subtitles"] = $subtitles;
-if (!is_null($egg)) $response["egg"] = $egg;
+$response['success'] = true;
+$response['comment'] = 'No errors';
+$response['filename'] = $filename;
+$response['title'] = $title;
+$response['mime'] = $mime;
+$response['source'] = $series;
+if (!is_null($song)) $response['song'] = $song;
+if (!is_null($subtitles)) $response['subtitles'] = $subtitles;
+if (!is_null($egg)) $response['egg'] = $egg;
 
 // Finish reply
 output($response);
