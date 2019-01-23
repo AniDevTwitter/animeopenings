711706aa716ba86b79977b3aa8b34837d6f24b18
Yay295 <Yay295@gmail.com>
2019-01-22 23:18:15 -0600

    slightly better font weight and style detection

--- encode/fontConverter.alt.py
+++ encode/fontConverter.alt.py
@@ -93,6 +93,7 @@ for index, file in enumerate(files,1):
 			font.generate(os.path.join(todir, newfilename + '.woff2'))
 			names = {font.cidfamilyname, font.cidfontname, font.cidfullname, font.familyname, font.fontname, font.fullname, font.default_base_filename, font.fondname}
 			names.update(string for language, strid, string in font.sfnt_names if strid in {'Family', 'Fullname', 'PostScriptName'})
+			subfamily = next((string for language, strid, string in font.sfnt_names if strid == 'SubFamily'), 'Regular')
 			font.close()
 		if debugFontConverter:
 			errlog.append(stderr.capturedtext.strip())
@@ -116,52 +117,73 @@ for index, file in enumerate(files,1):
 						names.add(macName)
 					names.add(errors[i+1][0].split(':')[1].strip())
 
-		# Add the font file name to the list of names for this font.
-		names.add(os.path.splitext(os.path.basename(file))[0])
+		# One of my fonts has the filename 'calibri' even though it is not
+		# Calibri. The actual Calibri font does not have a lowercase 'c',
+		# so we can just remove it. Also remove blank names.
+		names.discard('calibri')
 		names.discard(None)
 		names.discard('')
 
-		# One of my fonts has the filename 'calibri' even though it is not
-		# Calibri. The actual Calibri font does not have a lowercase 'c',
-		# so we can just remove it.
-		names.discard('calibri')
-
 		# Find font weight and style.
-		weight = ''
-		italic = False
-		oblique = False
-		for name in names:
-			nlower = name.lower()
+		if True:
+			def check_weight_style(name):
+				nlower = name.lower()
 
-			if 'thin' in nlower or 'hairline' in nlower:
-				weight = '100'
-			elif 'extralight' in nlower or 'ultralight' in nlower:
-				weight = '200'
-			elif 'light' in nlower:
-				weight = '300'
-			elif 'normal' in nlower:
-				weight = '400'
-			elif 'medium' in nlower:
-				weight = '500'
-			elif 'semibold' in nlower or 'demibold' in nlower or 'demi' in nlower:
-				weight = '600'
-			elif 'extrabold' in nlower or 'ultrabold' in nlower:
-				weight = '800'
-			elif 'bold' in nlower:
-				weight = '700'
-			elif 'black' in nlower or 'heavy' in nlower:
-				weight = '900'
+				weight = ''
+				italic = False
+				oblique = False
 
-			if 'italic' in nlower: italic = True
-			elif 'oblique' in nlower: oblique = True
+				if 'thin' in nlower or 'hairline' in nlower:
+					weight = '100'
+				elif 'extralight' in nlower or 'ultralight' in nlower:
+					weight = '200'
+				elif 'light' in nlower: # includes 'semilight'
+					weight = '300'
+				elif 'normal' in nlower:
+					weight = '400'
+				elif 'medium' in nlower:
+					weight = '500'
+				elif 'semibold' in nlower or 'demibold' in nlower or 'demi' in nlower:
+					weight = '600'
+				elif 'extrabold' in nlower or 'ultrabold' in nlower:
+					weight = '800'
+				elif 'bold' in nlower:
+					weight = '700'
+				elif 'black' in nlower or 'heavy' in nlower:
+					weight = '900'
 
-		weightstyle = ''
-		if weight:
-			weightstyle += '\tfont-weight: ' + weight + ';\n'
-		if italic:
-			weightstyle += '\tfont-style: italic;\n'
-		elif oblique:
-			weightstyle += '\tfont-style: oblique;\n'
+				if 'italic' in nlower: italic = True
+				elif 'oblique' in nlower: oblique = True
+
+				return weight, italic, oblique
+
+			# Check All Names
+			weight = ''
+			italic = False
+			oblique = False
+			for name in names:
+				weight, i, o = check_weight_style(name)
+				italic |= i
+				oblique |= o
+
+			# Check SubFamily
+			# This is usually not set correctly, so we only use it as a backup.
+			if subfamily != 'Regular':
+				w, i, o = check_weight_style(subfamily)
+				weight = weight or w
+				italic |= i and not oblique
+				oblique |= o
+
+			weightstyle = ''
+			if weight:
+				weightstyle += '\tfont-weight: ' + weight + ';\n'
+			if italic:
+				weightstyle += '\tfont-style: italic;\n'
+			elif oblique:
+				weightstyle += '\tfont-style: oblique;\n'
+
+		# Add the font file name to the list of names for this font.
+		names.add(os.path.splitext(os.path.basename(file))[0])
 
 		# Append @font-face for this font.
 		css = ''
