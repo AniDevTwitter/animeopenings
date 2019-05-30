d633d2f901951a5ef4d627a1f1424439417c408c
Yay295 <Yay295@gmail.com>
2019-05-29 21:12:00 -0500

    fix printing unicode to stdout

--- encode/subtitleConverter.py
+++ encode/subtitleConverter.py
@@ -368,5 +368,6 @@ def convert(lines, offset=0):
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
-		for line in convert([line.strip() for line in open(sys.argv[1], encoding='utf8') if line], int(sys.argv[2]) if len(sys.argv) > 2 else 0):
-			print(line)
+		with open(sys.argv[1], encoding='utf8') as file, open(1, 'w', encoding='utf8', closefd=False) as stdout:
+			for line in convert([line.strip() for line in file if line], int(sys.argv[2]) if len(sys.argv) > 2 else 0):
+				print(line, file=stdout)
